e1dc3012ced981a03e7e09616b7cb6da5bc5fdb0
yend724 <yend9524@gmail.com>
2022-04-23 16:08:07 +0900

    Update init animations

--- src/assets/js/index.ts
+++ src/assets/js/index.ts
@@ -6,41 +6,34 @@ const init = () => {
   bgInit();
 
   // init animation
-  const targetContainers = document.querySelectorAll(".js-split-anime")!;
-  targetContainers.forEach(targetContainer => {
-    const targetString = targetContainer.textContent || "";
-    const splitTexts = targetString.split("");
-    const newHtml = splitTexts.reduce((acc, cur) => {
-      return (acc += `<span>${cur}</span>`);
-    }, "");
-    targetContainer.innerHTML = newHtml;
-    targetContainer.querySelectorAll(":scope > span").forEach((item, i) => {
-      gsap.from(item, {
-        opacity: 0,
-        rotation: -90,
-        yPercent: -200,
-        ease: "bounce",
-        duration: 0.5,
-        delay: i * 0.1 + 0.2,
-      });
-    });
-  });
-
-  gsap.set(".js-split-anime", {
+  const initTextTl = gsap.timeline();
+  initTextTl.to("html", {
+    "--line-translateX": 0,
+    duration: 0.3,
+    ease: "power3.out",
+    delay: 0.2,
+  }).set(".js-slide-anime > .text", {
     opacity: 1,
+  }).to("html", {
+    "--line-translateX": 100,
+    ease: "power3.out",
+    duration: 0.3,
+    delay: 0.3,
   });
 
   gsap.to(".main", {
     opacity: 1,
-    duration: 0.5,
+    webkitFilter: "blur(0px)",
+    duration: 0.3,
+    ease: "power3.out",
     delay: 1.5,
   });
   gsap.to(".bg", {
     opacity: 1,
     scale: 1,
-    rotate: 0,
-    webkitFilter: "blur(" + 0 + "px)",
-    duration: 0.5,
+    webkitFilter: "blur(0px)",
+    duration: 0.3,
+    ease: "power3.out",
     delay: 1.5,
   });
 };
