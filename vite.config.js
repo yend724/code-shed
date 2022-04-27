ecb79a907ca05c30055621fa315fc2223eba72f0
yend724 <yend9524@gmail.com>
2022-04-27 16:47:38 +0900

    Add handlebars

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
 import { resolve } from "path";
+import { defineConfig } from "vite";
+import handlebars from "vite-plugin-handlebars";
 
-const root = 'src'
+const root = "src";
 export default defineConfig({
   root: resolve(__dirname, root),
   build: {
@@ -9,8 +10,17 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, root, "index.html"),
-        "threejs-raycaster": resolve(__dirname, root, "code/threejs-raycaster/index.html"),
+        "threejs-raycaster": resolve(
+          __dirname,
+          root,
+          "code/threejs-raycaster/index.html"
+        ),
       },
     },
   },
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "src", "partials"),
+    }),
+  ],
 });
