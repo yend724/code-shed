548e675db8945568436aa42e37f8a956318c32fe
yend724 <yend9524@gmail.com>
2022-04-28 14:39:25 +0900

    Update setWindowSize

--- src/assets/js/modules/setWindowSize.ts
+++ src/assets/js/modules/setWindowSize.ts
@@ -1,7 +1,7 @@
 const setWindowSize = () => {
 	const windowW = window.innerWidth;
 	const windowH = window.innerHeight;
-	document.documentElement.style.setProperty("--window-w-full", windowW.toString()+"px");
-	document.documentElement.style.setProperty("--window-h-full", windowH.toString()+"px");
+	document.documentElement.style.setProperty("--window-w-full", `${windowW}px`);
+	document.documentElement.style.setProperty("--window-h-full", `${windowH}px`);
 }
 export default setWindowSize;
