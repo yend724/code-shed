b43bb6a01c022e13514d3f25ede885fb1fa550df
yend724 <yend9524@gmail.com>
2022-04-28 22:53:33 +0900

    Update raycaster

--- src/code/threejs-raycaster/script.ts
+++ src/code/threejs-raycaster/script.ts
@@ -15,16 +15,16 @@ const init = () => {
   renderer.setSize(canvas.clientWidth, canvas.clientWidth / aspect);
   renderer.setPixelRatio(window.devicePixelRatio);
 
-  const planeGeometory = new THREE.PlaneGeometry(10, 10);
-  const planeMaterial = new THREE.MeshNormalMaterial();
+  const boxGeometory = new THREE.BoxGeometry(6, 6, 6);
+  const boxMaterial = new THREE.MeshNormalMaterial();
 
-  const planes = Array.from({ length: 3 }).map((_, i) => {
-    const plane = new THREE.Mesh(planeGeometory, planeMaterial);
-    plane.position.x = (i - 1) * 11;
-    return plane;
+  const boxes = Array.from({ length: 3 }).map((_, i) => {
+    const box = new THREE.Mesh(boxGeometory, boxMaterial);
+    box.position.x = (i - 1) * 11;
+    return box;
   });
 
-  scene.add(...planes);
+  scene.add(...boxes);
 
   const camera = new THREE.PerspectiveCamera(
     45,
@@ -43,11 +43,13 @@ const init = () => {
   });
 
   const pointer = new THREE.Vector2(-9999, -9999);
-  const pointerEvent = typeof window.ontouchstart === "undefined" ? "pointermove" : "pointerenter";
+  const pointerEvent =
+    typeof window.ontouchstart === "undefined" ? "pointermove" : "pointerenter";
+
   canvas.addEventListener(pointerEvent, event => {
     const element = event.currentTarget as HTMLCanvasElement;
-    const x = event.clientX - element.offsetLeft;
-    const y = event.clientY - element.offsetTop;
+    const x = event.clientX - element.offsetLeft + window.scrollX;
+    const y = event.clientY - element.offsetTop + window.scrollY;
     const w = element.offsetWidth;
     const h = element.offsetHeight;
 
@@ -61,6 +63,8 @@ const init = () => {
     const intersects = raycaster.intersectObjects(scene.children);
 
     if (intersects.length > 0) {
+      intersects[0].object.rotation.x += 0.01;
+      intersects[0].object.rotation.y += 0.01;
       intersects[0].object.rotation.z += 0.01;
     }
 
