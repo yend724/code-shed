ffd0a43e2b5fc75a9240fbff8e60a3d19c0aaa74
yend724 <yend9524@gmail.com>
2022-04-30 02:38:06 +0900

    Update pug config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import {render} from "pug"
 import vitePluginPug from "./plugins/vite-plugin-pug";
 
+const escapeHtml = (string) => {
+  return string.replace(/[&'`"<>]/g, (match) => {
+    return {
+      '&': '&amp;',
+      "'": '&#x27;',
+      '`': '&#x60;',
+      '"': '&quot;',
+      '<': '&lt;',
+      '>': '&gt;',
+    }[match]
+  });
+}
 const root = "src"
 export default defineConfig({
   root: resolve(__dirname, root),
@@ -26,6 +39,12 @@ export default defineConfig({
   plugins: [
     vitePluginPug({
       basedir: resolve(__dirname, root),
+      filters : {
+        'code-html':  (text) => {
+          const rendered = render(text, {pretty: true});
+          return escapeHtml(rendered).trim();
+        }
+      }
     }),
   ],
 });
