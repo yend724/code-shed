043cdacf2f47ed523c8c12b2dfdbeec74584fbea
yend724 <yend9524@gmail.com>
2022-05-01 12:58:04 +0900

    Add css text-shadow 3d

--- src/assets/js/common.ts
+++ src/assets/js/common.ts
@@ -1,11 +1,15 @@
 import "destyle.css";
 import "/assets/css/style.scss";
-import "prismjs/themes/prism-funky.min.css";
 import Prism from "prismjs";
+import 'prismjs/components/prism-pug.min.js'
+import 'prismjs/components/prism-scss.min.js'
+import 'prismjs/components/prism-typescript.min.js'
+import "prismjs/themes/prism-funky.min.css";
 import setWindowSize from "./modules/setWindowSize";
 
 const init = () => {
   setWindowSize();
+  Prism.manual = true;
   Prism.highlightAll();
 };
 
