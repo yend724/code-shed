bdee4f9dbf5ce47d80381277db51d6bddcbe63e6
yend724 <yend9524@gmail.com>
2022-05-04 22:53:08 +0900

    Update Styles

--- src/assets/js/code.ts
+++ src/assets/js/code.ts
@@ -13,13 +13,14 @@ const init = () => {
   Prism.manual = true;
   Prism.highlightAll();
 
-  const codeInfo = document.getElementById("code-info") as HTMLElement;
+  const wrapper = document.getElementById("js-wrapper") as HTMLElement;
   const slideBtn = document.getElementById("js-button-slide") as HTMLButtonElement;
 
   slideBtn.addEventListener("click", () => {
-    const state = codeInfo.dataset.open as "open" | "close";
-    const next = state === "open" ? "close" : "open";
-    codeInfo.dataset.open = next
+    const state = slideBtn.getAttribute("aria-expanded") as "true" | "false";
+    const next = state === "true" ? "false" : "true";
+    slideBtn.setAttribute("aria-expanded", next)
+    wrapper.setAttribute("aria-hidden", state)
   })
 };
 window.addEventListener("resize", setWindowSize);
