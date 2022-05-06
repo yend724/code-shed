e03df585268b240022b650c7d21f7fa57cb7a921
yend724 <yend9524@gmail.com>
2022-05-06 22:29:32 +0900

    Update Modal Class

--- src/assets/js/code.ts
+++ src/assets/js/code.ts
@@ -7,22 +7,16 @@ import "prismjs/components/prism-scss.min.js";
 import "prismjs/components/prism-typescript.min.js";
 import "prismjs/themes/prism-funky.min.css";
 import setWindowSize from "./modules/setWindowSize";
+import { Modal } from "./modules/modal";
 
 const init = () => {
   setWindowSize();
   Prism.manual = true;
   Prism.highlightAll();
 
-  const wrapper = document.getElementById("js-wrapper") as HTMLElement;
-  const slideBtn = document.getElementById("js-button-slide") as HTMLButtonElement;
-
-  slideBtn.addEventListener("click", () => {
-    const state = slideBtn.getAttribute("aria-expanded") as "true" | "false";
-    const next = state === "true" ? "false" : "true";
-    const label = next === "true" ? "メニューを閉じる" : "メニューを開く";
-    slideBtn.setAttribute("aria-expanded", next)
-    slideBtn.setAttribute("aria-label", label)
-    wrapper.setAttribute("aria-hidden", state)
+  new Modal({
+    modalTarget: "js-wrapper",
+    trigger: "js-button-slide"
   })
 };
 window.addEventListener("resize", setWindowSize);
