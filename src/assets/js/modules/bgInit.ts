e0c763e0f0ab8ef52fcb26a1ec4432727f27fff9
yend724 <yend9524@gmail.com>
2022-05-20 11:33:26 +0900

    Update toppage

--- src/assets/js/modules/bgInit.ts
+++ src/assets/js/modules/bgInit.ts
@@ -1,4 +1,62 @@
 import * as THREE from "three";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
+const pageData = [
+  {
+    pageTitle: "線形補間を用いた追従エフェクト",
+    path: "/code/scroll-parallax-lerp/",
+    date: "2022-05-16",
+    category: "code",
+  },
+  {
+    pageTitle: "スクロールイベントによるパララックス",
+    path: "/code/scroll-parallax-on-scrollevent/",
+    date: "2022-05-15",
+    category: "code",
+  },
+  {
+    pageTitle: "Three.jsとShaderを使って画像をゆらゆらさせる",
+    path: "/code/threejs-shader-texture-wave/",
+    date: "2022-05-14",
+    category: "code",
+  },
+  {
+    pageTitle: "3Dホバーエフェクト（カードUI）",
+    path: "/code/card-3d-hover-effect/",
+    date: "2022-05-06",
+    category: "code",
+  },
+  {
+    pageTitle: "マウスによるパララックス（フルスクラッチ）",
+    path: "/code/mouse-parallax-full-scratch/",
+    date: "2022-05-05",
+    category: "code",
+  },
+  {
+    pageTitle: "SVGでstrokeのテキストアニメーション",
+    path: "/code/text-stroke-animation/",
+    date: "2022-05-05",
+    category: "code",
+  },
+  {
+    pageTitle: "CSSのtext-shadowでテキストの3D表現",
+    path: "/code/css-textshadow-3d/",
+    date: "2022-05-01",
+    category: "code",
+  },
+  {
+    pageTitle: "ドーナツセレクタ",
+    path: "/code/donut-selector/",
+    date: "2022-05-02",
+    category: "code",
+  },
+  {
+    pageTitle: "Three.jsのRaycasterで交差をしらべる",
+    path: "/code/threejs-raycaster/",
+    date: "2022-05-01",
+    category: "code",
+  },
+];
 
 const bgInit = () => {
   const scene = new THREE.Scene();
@@ -11,32 +69,56 @@ const bgInit = () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
 
-  // const axes = new THREE.AxesHelper(20);
-  // scene.add(axes);
-
-  const cubeGeometory = new THREE.BoxGeometry(1, 1, 1);
-  const cubeMaterial = new THREE.MeshPhysicalMaterial({
-    color: 0x424242,
-  });
-  const range = 100;
-  const cubes = Array.from({ length: 125 }).map(_ => {
-    const cube = new THREE.Mesh(cubeGeometory, cubeMaterial);
-    const speed = 1 - Math.random() * 0.5;
-    const scale = 1 + Math.random() * 5;
-    const posX = (Math.random() - 0.5) * range;
-    const posY = (Math.random() - 0.5) * range;
-    const posZ = (Math.random() - 0.5) * range;
-    const rotX = Math.random() * Math.PI * 2;
-    const rotY = Math.random() * Math.PI * 2;
-    const rotZ = Math.random() * Math.PI * 2;
-    cube.position.set(posX, posY, posZ);
-    cube.rotation.set(rotX, rotY, rotZ);
-    cube.scale.set(scale, scale, scale);
-    scene.add(cube);
-    return {
-      target: cube,
-      speed: speed,
+  let title: THREE.Mesh<TextGeometry, THREE.MeshNormalMaterial>;
+  let texts: {
+    target: THREE.Mesh<TextGeometry, THREE.MeshNormalMaterial>;
+    random: {
+      x: number;
+      y: number;
+      z: number;
     };
+    speed: number;
+  }[] = [];
+  const fontloader = new FontLoader();
+  fontloader.load("/assets/fonts/BIZUDPGothic_Bold.json", font => {
+    const titleGeometory = new TextGeometry("Code Shed", {
+      font: font,
+      size: 0.5,
+      height: 0.3,
+    });
+    titleGeometory.center();
+    const titleMaterial = new THREE.MeshNormalMaterial();
+    title = new THREE.Mesh(titleGeometory, titleMaterial);
+    title.position.set(0, 0, 0);
+    scene.add(title);
+
+    texts = pageData.map(v => {
+      const random = {
+        x: Math.random() * Math.PI * 2,
+        y: Math.random() * Math.PI * 2,
+        z: Math.random() * Math.PI * 2,
+      };
+      const speed = Math.random() + 0.2;
+      const geometry = new TextGeometry(v.pageTitle, {
+        font: font,
+        size: 0.25,
+        height: 0.15,
+      });
+      geometry.center();
+      const material = new THREE.MeshNormalMaterial();
+      const mesh = new THREE.Mesh(geometry, material);
+      mesh.position.set(
+        Math.cos(random.x) * 5,
+        Math.sin(random.y) * 5,
+        Math.sin(random.z) * 5
+      );
+      scene.add(mesh);
+      return {
+        target: mesh,
+        random: random,
+        speed: speed,
+      };
+    });
   });
 
   const camera = new THREE.PerspectiveCamera(
@@ -45,19 +127,15 @@ const bgInit = () => {
     0.1,
     500
   );
-  camera.position.set(0, 0, 75);
+  camera.position.set(0, 0, 25);
   camera.lookAt(scene.position);
 
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-  directionalLight.position.set(0, 100, 100);
-  scene.add(directionalLight);
+  // const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+  // directionalLight.position.set(0, 50, 100);
+  // scene.add(directionalLight);
 
-  const pointLight = new THREE.PointLight(0x1a237e);
-  pointLight.position.set(0, 0, 0);
-  scene.add(pointLight);
-
-  let mouseX = 0,
-    mouseY = 0;
+  let mouseX = 0;
+  let mouseY = 0;
   window.addEventListener("pointermove", e => {
     mouseX = e.clientX / window.innerWidth - 0.5;
     mouseY = e.clientY / window.innerHeight - 0.5;
@@ -66,30 +144,38 @@ const bgInit = () => {
   window.addEventListener("resize", () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    camera.lookAt(scene.position);
+    camera.lookAt(0, 0, 0);
     renderer.setSize(window.innerWidth, window.innerHeight);
   });
 
   let previousTimestamp = 0;
   const renderScene = (timestamp?: number) => {
-    timestamp = timestamp || 0;
-    const elapsed = (timestamp - previousTimestamp) / 1000;
-    previousTimestamp = timestamp;
+    const ts = timestamp || 0;
+    const elapsed = (ts - previousTimestamp) / 1000;
+    previousTimestamp = ts;
 
-    cubes.forEach((cube, i) => {
-      const { target, speed } = cube;
-      const ts = timestamp || 0;
-      target.position.x += Math.sin(ts / 1000 + i) * 0.03;
-      target.position.y += Math.cos(ts / 1000 + i) * 0.05;
-      target.position.z += Math.sin(ts / 1000 + i) * 0.07;
-      (["x", "y", "z"] as ("x" | "y" | "z")[]).forEach(r => {
-        target.rotation[r] += elapsed * speed;
+    if (title) {
+      title.rotation.x += elapsed;
+      title.rotation.y -= elapsed;
+      title.rotation.z += elapsed;
+    }
+    if (texts.length > 0) {
+      texts.forEach(text => {
+        const { target, random, speed } = text;
+        target.position.x = Math.cos(random.x + (ts * speed) / 1200) * 5.0;
+        target.position.y = Math.sin(random.y + (ts * speed) / 1200) * 5.0;
+        target.position.z = Math.sin(random.z + (ts * speed) / 1200) * 5.0;
+
+        (["x", "y", "z"] as ("x" | "y" | "z")[]).forEach(r => {
+          target.rotation[r] +=
+            (elapsed * (random.x + random.y + random.z)) / 3 / 10;
+        });
       });
-    });
+    }
 
     const cameraPos = {
-      x: mouseX * 30,
-      y: mouseY * 30 * -1,
+      x: mouseX * 10,
+      y: mouseY * 10 * -1,
     };
     camera.position.x += (cameraPos.x - camera.position.x) * 0.03;
     camera.position.y += (cameraPos.y - camera.position.y) * 0.03;
